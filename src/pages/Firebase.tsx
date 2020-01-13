13c2ce661a0bce207fb17adb0c02d216477ceca3
KevinKipkemei <42734616+KevinKipkemei@users.noreply.github.com>
2020-01-13 17:48:58 +0300

    Update Firebase.tsx

--- src/pages/Firebase.tsx
+++ src/pages/Firebase.tsx
@@ -2,6 +2,14 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseApp = firebase.initializeApp ({
+    apiKey: "AIzaSyCkE-xmQualBe0m713t3wf8zPr5U5SChYE",
+    authDomain: "field-d538a.firebaseapp.com",
+    databaseURL: "https://field-d538a.firebaseio.com",
+    projectId: "field-d538a",
+    storageBucket: "field-d538a.appspot.com",
+    messagingSenderId: "772294768735",
+    appId: "1:772294768735:web:1e2c558a981215af6583ea",
+    measurementId: "G-40ZLQMLCEE"  
  
  });
 
